98d1e9d45176f1c23813ded8096e2a4a18e6a219
Aaron Castells <aaroncastells@gmail.com>
2025-12-17 12:15:55 +0100

    feat: add ESLint configuration for Storybook and update dependencies

---
+++ apps/storybook/eslint.config.js
@@ -0,0 +1,44 @@
+// ESLint flat config for @echo/storybook
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "storybook-static", "node_modules"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      // React Hooks rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      // Allow unused vars that start with underscore
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      // Relax some rules for component library
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  // Storybook specific config
+  ...storybook.configs["flat/recommended"],
+]);
